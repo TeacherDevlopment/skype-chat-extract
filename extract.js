afd829c02fca6bdc203de11dbe0d8b9e8d07ef82
jakerella <jordan@jordankasper.com>
2014-03-29 14:58:18 -0500

    Added various option parsing and basic help/usage info

--- extract.js
+++ extract.js
@@ -5,9 +5,11 @@ var fs = require('fs'),
     defaultFileLocation = '{{home}}/.Skype/{{username}}/main.db',
     options = {
         dbfile: null,
-        exportfile: './output/' + (new Date()).getTime() + '.csv',
         username: null,
-        chatName: null,
+        chatname: null,
+        limit: 1000,
+        maxage: null,
+        exportfile: './output/' + (new Date()).getTime() + '.csv',
         fields: ['timestamp', 'convo_id', 'author', 'body_xml'],
         includeHeader: true,
         newName: null,
@@ -19,6 +21,7 @@ var fs = require('fs'),
  * @return {void}
  */
 function main() {
+    console.log("\nSkype Chat Extract\n".blue.bold);
 
     var o = processOptions();
 
@@ -51,7 +54,8 @@ function main() {
  * @return {Object} The integrated options
  */
 function processOptions() {
-    var o = options,
+    var d,
+        o = options,
         args = process.argv.slice(2);
 
     args.forEach(function(val) {
@@ -60,8 +64,25 @@ function processOptions() {
 
         } else if (/^username=/.test(val)) {
             o.username = val.split(/\=/)[1];
-        }
 
+        } else if (/^chatname=/.test(val)) {
+            o.chatname = val.split(/\=/)[1];
+
+        } else if (/^limit=/.test(val)) {
+            o.limit = Number(val.split(/\=/)[1]) || 1000;
+
+        } else if (/^maxage=/.test(val)) {
+            d = new Date(val.split(/\=/)[1]);
+            o.maxage = (d && d.getTime()) || null;
+
+        } else if (/^exportfile=/.test(val)) {
+            o.exportfile = val.split(/\=/)[1];
+
+        } else if (/^-h$/.test(val) || /^help$/.test(val)) {
+            // If the user is request help, show it and exit
+            printUsage();
+            process.exit(0);
+        }
     });
 
     if (!o.dbfile && o.username) {
@@ -76,9 +97,22 @@ function processOptions() {
 }
 
 function printUsage() {
-
-    console.log();
-
+    console.log(
+        "This script extracts chat messages from your local Skype\n" +
+        "storage and creates a comma-separated version of the file\n" +
+        "(a .csv) useful for importing into other systems." +
+        "\n\n" +
+        "    Basic Example:" +
+        "  ~$ node extract.js username=jordan chatname=\"Project Chat\"".yellow +
+        "\n\n" +
+        "Available options:\n" +
+        "  username    Your Skype username; used to find your Skype DB file (optional, use either this OR 'dbfile')\n" +
+        "  dbfile      The location of your Skype SQLite database file (optional, use either this OR 'username')\n" +
+        "  chatname    The name of the chat you wish to export (can be a partial name) (required)\n" +
+        "  limit       Limit the number of chat messages returned (optional, defaults to 1000)\n" +
+        "  maxage      The oldest chat message to retrieve; can be date (YYYY-MM-DD) or timestamp (optional)\n" +
+        "  exportfile  Name of the file to export to (optional, defaults to output/{timestamp}.csv)\n"
+    );
 }
 
 
